f0a677f084da3889f1122abde919f0b2e3f897f1
Liaw Lim <liaw.lim@suppo.fr>
2018-01-09 13:46:56 +0100

    De nouveaux tests

--- tests/Test.php
+++ tests/Test.php
@@ -1,10 +1,38 @@
 <?php
 
+use DeGraciaMathieu\Riddler\Password;
+use DeGraciaMathieu\Riddler\Criterias;
+use DeGraciaMathieu\Riddler\Occurences;
+
 class Test extends \PHPUnit\Framework\TestCase
 {
-	/** @test */
+    /** @test */
     public function example()
     {
-    	return $this->assertTrue(true);
+        return $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function emptyPassword()
+    {
+        $pw = new Password();
+
+        $str = $pw->generate();
+
+        $this->assertEmpty($str);
+    }
+
+    /** @test */
+    public function digitPasswordOnly()
+    {
+        $pw = new Password();
+
+        $pw->addCriteria(new Criterias\Digit(), new Occurences\Strict(10));
+
+        $str = $pw->generate();
+
+        $this->assertNotEmpty($str);
+
+        $this->assertRegExp('/\d{10}/', $str);
     }
 }
