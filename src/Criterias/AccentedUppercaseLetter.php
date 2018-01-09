672a1d3da07c4a4e1edb4cf51f4d4660b476f02f
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-01-09 13:24:48 -0800

    Création du critère AccentedUppercaseLetter

---
+++ src/Criterias/AccentedUppercaseLetter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DeGraciaMathieu\Riddler\Criterias;
+    
+class AccentedUppercaseLetter
+{
+    public function handle()
+    {
+        return ['À','Á','Â','Ã','Ä','Å','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý'];
+    }
+}
