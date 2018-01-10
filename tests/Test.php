3098af0443d4ed70931a1ca1276c0fa0885c39c6
DeGraciaMathieu <contact@degracia-mathieu.fr>
2018-01-10 12:16:13 -0800

    Ajout de plusieurs tests

--- tests/Test.php
+++ tests/Test.php
@@ -2,16 +2,11 @@
 
 use DeGraciaMathieu\Riddler\Password;
 use DeGraciaMathieu\Riddler\Criterias;
+use DeGraciaMathieu\Riddler\Formats;
 use DeGraciaMathieu\Riddler\Occurences;
 
 class Test extends \PHPUnit\Framework\TestCase
 {
-    /** @test */
-    public function example()
-    {
-        return $this->assertTrue(true);
-    }
-
     /** @test */
     public function emptyPassword()
     {
@@ -20,6 +15,34 @@ class Test extends \PHPUnit\Framework\TestCase
         $str = $pw->generate();
 
         $this->assertEmpty($str);
+    }
+
+    /** @test */
+    public function letterPasswordOnly()
+    {
+        $pw = new Password();
+
+        $pw->addCriteria(new Criterias\Letter(), new Occurences\Strict(10));
+
+        $str = $pw->generate();
+
+        $this->assertNotEmpty($str);
+
+        $this->assertRegExp('/[a-z]/', $str);
+    }
+
+    /** @test */
+    public function FormatOnly()
+    {
+        $pw = new Password();
+
+        $pw->addFormat(new Formats\LongDigit());
+
+        $str = $pw->generate();
+
+        $this->assertNotEmpty($str);
+
+        $this->assertRegExp('/\d{30}/', $str);
     }
 
     /** @test */
@@ -34,5 +57,37 @@ class Test extends \PHPUnit\Framework\TestCase
         $this->assertNotEmpty($str);
 
         $this->assertRegExp('/\d{10}/', $str);
+    }    
+
+    /** @test */
+    public function strictOccurences()
+    {
+        $dictionary = ['a', 'b', 'c', 'd'];
+
+        $strict = new Occurences\Strict(5);
+        $result = $strict->parse($dictionary);
+
+        $this->assertTrue(is_array($result));
+        $this->assertEquals(5, count($result));
+        $this->assertContains($result[0], $dictionary);
+        $this->assertContains($result[1], $dictionary);
+        $this->assertContains($result[2], $dictionary);
+        $this->assertContains($result[3], $dictionary);
+    }
+
+    /** @test */
+    public function betweenOccurences()
+    {
+        $dictionary = ['a', 'b', 'c', 'd'];
+
+        $strict = new Occurences\Between(3, 5);
+        $result = $strict->parse($dictionary);
+
+        $this->assertTrue(is_array($result));
+        $this->assertTrue(count($result) >= 3);
+        $this->assertTrue(count($result) <= 5);
+        $this->assertContains($result[0], $dictionary);
+        $this->assertContains($result[1], $dictionary);
+        $this->assertContains($result[2], $dictionary);
     }
 }
